1d3d24e9a8d2c85f53c4d4cde638c848f206d44d
ParkJinwon1025 <jinwon2812@ubisam.com>
2026-01-14 15:45:38 +0900

    임시 디자인 및 코드 수정

--- sample1/src/pages/main.vue
+++ sample1/src/pages/main.vue
@@ -43,8 +43,8 @@
                 <!-- ⭐ 저장/취소 버튼 추가 (선택사항) -->
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn text="취소" @click="dialog = false"></v-btn>
                     <v-btn text="저장" color="primary" @click="saveUser"></v-btn>
+                    <v-btn text="취소" @click="dialog = false"></v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
@@ -61,7 +61,8 @@ export default {
             dialog: false,
             name: '',
             email: '',
-            phone: ''
+            phone: '',
+            currentUserUrl: ''
         }
     },
     mounted() {
@@ -87,11 +88,46 @@ export default {
                 this.name = user1.name;
                 this.email = user1.email;
                 this.phone = user1.phone;
+                this.currentUserUrl = user1._links.self.href;
                 this.dialog = true;
             }
         },
         clickDelete() {
             console.log('clickDelete method called');
+        },
+        saveUser(){
+
+            const userId = this.currentUserUrl.split('/').pop();
+
+            axios(
+                {
+                    method:'put',
+                    url:this.currentUserUrl,
+                    data:{
+                        name:this.name,
+                        email:this.email,
+                        phone:this.phone
+                    }
+                }
+            )
+            .then(response =>{
+                console.log("수정 완료!");
+                this.dialog = false;
+                return axios(
+                    {
+                        method: 'get',
+                        url: 'http://localhost:8080/api/users'
+                    }
+                );
+            }
+            )
+            .then(response => {
+                this.users = response.data._embedded.users;
+                console.log(this.users);
+            })
+            .catch(error =>{
+                console.error('Error updating user:', error);
+            });
         }
 
     }
